abbb7721a65d6eb26dd480e590a525cd0ebafc8e
Johnervan <johnervan@gmail.com>
2018-01-24 00:10:00 +0800

    fix error in platform api path

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   TELEGRAM_API_URL: "https://api.telegram.org/bot" + process.env.BOT_TOKEN,
   BOT_TOKEN: process.env.BOT_TOKEN,
   WEBHOOK_URL: `${process.env.PUBLIC_URL}/bot${process.env.BOT_TOKEN}`,
-  PLATFORM_URL: process.env.PLATFORM_URL,
+  PLATFORM_URL: `${process.env.PLATFORM_URL}/platform${process.env.BOT_TOKEN}`,
 }
