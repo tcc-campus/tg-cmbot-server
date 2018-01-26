4188dc36fd68bcab322c63440aee224422441a8a
Johnervan <johnervan@gmail.com>
2018-01-27 00:45:41 +0800

    push

---
+++ handlers/callback_query_handler.js
@@ -0,0 +1,20 @@
+function handleCallbackQuery(chatId, msgObj) {
+  const dateRange = dt_util.getDateRangeForThisMonth();
+  console.log("Date range for this month: " + JSON.stringify(dateRange));
+  pf_caller.getUpcomingEvents(dateRange.start_date, dateRange.end_date).then((result) => {
+    console.log(result.message);
+    const eventList = JSON.parse(result.body);
+    console.log(eventList);
+    evt_formatter.formatEventList(eventList).then(formattedEventList => {
+      msg_formatter.formatUpcomingMessage(formattedEventList).then((message) => {
+        tg_caller.sendMessage(chatId, message, {'parse_mode': 'markdown'}).then((result) => {
+          console.log(result.message);
+        }).catch((error) => {
+          console.log(error);
+        });
+      })
+    })
+  }).catch((error) => {
+    console.log(error);
+  })
+}
