15019d68df4efaf35c09c062e313c62d499fbc76
Johnervan <johnervan@gmail.com>
2018-06-03 01:34:18 +0800

    updates

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   TELEGRAM_API_URL: `https://api.telegram.org/bot${process.env.BOT_TOKEN}`,
   BOT_TOKEN: process.env.BOT_TOKEN,
-  WEBHOOK_URL: `${process.env.PUBLIC_URL_SECURE}/bot${process.env.BOT_TOKEN}`,
+  WEBHOOK_URL: `${process.env.PUBLIC_URL}/bot${process.env.BOT_TOKEN}`,
   PLATFORM_URL: `${process.env.PLATFORM_URL}/platform${process.env.BOT_TOKEN}`,
   CACHE_DURATION: 600,
   DEV_GROUP_ID: process.env.DEV_GROUP_ID,
--- utils/cacheUtil.js
+++ utils/cacheUtil.js
@@ -1,9 +1,5 @@
 let cacheProvider = require('../cacheProvider');
 
-const REPLY_TYPE = {
-  FEEDBACK: 'feedback_reply',
-}
-
 const CALLBACK_QUERY_TYPE = {
   UPCOMING_MONTH: 'upcoming_month_callback_query',
   UPCOMING_EVENT_DETAIL: 'upcoming_event_detail_callback_query',
@@ -15,7 +11,6 @@ function getCacheObj(cacheKey) {
 }
 
 module.exports = {
-  REPLY_TYPE,
   CALLBACK_QUERY_TYPE,
   getCacheObj,
 }
